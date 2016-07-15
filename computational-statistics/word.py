de43b9b77b3064886e36d593a788228bf820beed
liberaliscomputing <liberalis.computing@gmail.com>
2016-07-15 16:42:11 +0900

    use list comprehension for importing text

--- computational-statistics/word.py
+++ computational-statistics/word.py
@@ -6,13 +6,17 @@ from thinkbayes import Pmf
 pmf = Pmf()
 
 #generate word list from text
-fname = ''
-word_list = []
+fname = 'data/aristotle.txt'
+with open(fname) as f:
+	word_list = [line.rstrip().split() for line in f]
 
+print word_list
+'''
 #count words
 for word in word_list:
 	pmf.Incr(word, 1)
 
 #normalize word freqeuncies as probabilities
 pmf.Normalize()
+'''
 

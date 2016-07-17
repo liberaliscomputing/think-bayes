163bd5d564c2eecfcd44eda874e381447b07d37b
liberaliscomputing <liberalis.computing@gmail.com>
2016-07-18 02:58:34 +0900

    complete cookie.py

--- computational-statistics/cookie.py
+++ computational-statistics/cookie.py
@@ -5,3 +5,25 @@ from thinkbayes import Pmf
 #initialize constructor
 pmf = Pmf()
 
+bowls = {
+	'bowl_1': {
+		'prior': 0.5, 
+		'likelihood': 0.75
+	},
+	'bowl_2': {
+		'prior': 0.5, 
+		'likelihood': 0.5
+	}
+}
+
+#set prior and likelihood of each hypothesis
+for bowl, probs in bowls.items():
+	pmf.Set(bowl, probs['prior'])
+	pmf.Mult(bowl, probs['likelihood'])
+
+#divide by normalizing constant
+pmf.Normalize()
+
+for bowl in bowls:
+	print 'Probability of %s given cookie: %f' % (bowl, pmf.Prob(bowl))
+

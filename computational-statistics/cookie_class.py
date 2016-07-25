c1081947b02c4c8bfcc39fe594fa0690019fef3d
liberaliscomputing <liberalis.computing@gmail.com>
2016-07-26 00:40:39 +0900

    finalize modularization

--- computational-statistics/cookie_class.py
+++ computational-statistics/cookie_class.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 from thinkbayes import Pmf
@@ -22,24 +23,28 @@ class Cookie(Pmf):
 		likelihood = bowl[prior]
 		return likelihood		
 
-bowls = {
-		'bowl_1': {
-			'vanilla': 0.75,
-			'chocolate': 0.25
-		},
-		'bowl_2': {
-			'vanilla': 0.5,
-			'chocolate': 0.5
+if __name__ == '__main__':
+	#define data
+	bowls = {
+			'bowl_1': {
+				'vanilla': 0.75,
+				'chocolate': 0.25
+			},
+			'bowl_2': {
+				'vanilla': 0.5,
+				'chocolate': 0.5
+			}
 		}
-	}
 
-#initialize constructor
-pmf = Cookie(bowls)
+	#initialize constructor
+	pmf = Cookie(bowls)
 
-#set likelihood of each hypothesis
-prior = 'vanilla'
-pmf.Update(prior)
+	#set likelihood of each hypothesis
+	#prior = 'vanilla'
+	priors = ['vanilla', 'chocolate', 'vanilla']
+	for prior in priors:
+		pmf.Update(prior)
 
-for hypo, prob in pmf.Items():
-	print 'Probability of %s given %s cookie: %f' % (hypo, prior, prob)
+	for hypo, prob in pmf.Items():
+		print 'Probability of %s given %s cookie: %f' % (hypo, prior, prob)
 
